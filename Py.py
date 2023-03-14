dd9a0a84b02a0ca8781238428d6f14c00407a2a5
slimiii <slimiii.gg@gmail.com>
2023-03-14 09:36:21 +0100

    Funktion Hinzugefügt

--- Py.py
+++ Py.py
@@ -1,1 +1,9 @@
-print("Lol, das ist da wohl nötig")
+import time
+
+current_time = time.time
+def printTime():
+    print("Es ist " + current_time + " Uhr./n")
+    print("Wie viel uhr ist es in Zehn stunden???")
+    print(input("In Zehn stunden is es: "))
+    
+    
